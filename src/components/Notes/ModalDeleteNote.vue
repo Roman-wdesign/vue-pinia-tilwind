a5475a4371e04ef881d7d5ad3961b228e0de7258
Roman frontend <mail@wbcplace.ru>
2022-10-07 11:53:21 +0700

    Add handleKeyboard, onMounted, onUnmounted in ModalDeleteNote.vue

--- src/components/Notes/ModalDeleteNote.vue
+++ src/components/Notes/ModalDeleteNote.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import { onClickOutside } from "@vueuse/core";
+import {useStoreNotes} from '@/stores/storeNotes'
 
 /*
 props
@@ -9,6 +10,10 @@ const props = defineProps({
   modelValue: {
     type: Boolean,
     default: false
+  },
+  noteId: {
+    type: String,
+    required: true
   }
 });
 /*
@@ -23,16 +28,39 @@ const closeModal = () => {
 
 const modalCardRef = ref(null);
 onClickOutside(modalCardRef, closeModal);
+
+/*
+keyboard
+ */
+const handleKeyboard = e => {
+  if (e.key === "Escape") closeModal();
+};
+
+onMounted(() => {
+  document.addEventListener("keyup", handleKeyboard);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("keyup", handleKeyboard);
+});
+/*
+store
+ */
+
+const storeNotes= useStoreNotes()
+
 </script>
 
 <template>
+
   <div class="relative z-10"
        aria-labelledby="modal-title"
        role="dialog"
        aria-modal="true"
 
   >
-    <div ref="modalCardRef" class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+    <div ref="modalCardRef"
+         class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
 
     <div class="fixed  inset-0 z-10 overflow-y-auto">
       <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
@@ -52,17 +80,19 @@ onClickOutside(modalCardRef, closeModal);
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">Delete note</h3>
                 <div class="mt-2">
-                  <p class="text-sm text-gray-500">Are you sure you want to delete note? This action cannot be undone.</p>
+                  <p class="text-sm text-gray-500">Are you sure you want to delete note? This action cannot be
+                    undone.</p>
                 </div>
               </div>
             </div>
           </div>
           <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
             <button type="button"
-                    @click="closeModal"
+                    @click="storeNotes.deleteNote(noteId)"
                     class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">
               Delete
             </button>
+
             <button type="button"
                     @click="closeModal"
                     class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -56,6 +56,7 @@ const modals = reactive({
     <ModalDeleteNote
       v-if="modals.deleteNote"
       v-model="modals.deleteNote"
+      :noteId="note.id"
     />
   </div>
 </template>
